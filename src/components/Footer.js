e361ddfdd0d71b30d3a06f4a2a3ef3df864900d6
Melaza6 <cueroleo6@gmail.com>
2023-04-04 16:59:16 -0700

    adjusted indentation on Header.js and Footer.js files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,27 +4,34 @@ import { Nav, NavItem, NavLink } from 'reactstrap'
 const Footer = () => {
   return (
     <>
-        <Nav pills>
-      <NavItem>
-        <NavLink
-          active
-          href="#"
-        >
-          Our Cats
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="#">
-         Create a Cat
-        </NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href="#">
-          Are you a Cat?
-        </NavLink>
-      </NavItem>
-    </Nav>
-</>
+      <Nav pills>
+
+        <NavItem>
+          <NavLink active href="#">
+
+            Our Cats
+
+          </NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink href="#">
+
+            Create a Cat
+
+          </NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink href="#">
+
+            Are you a Cat?
+
+          </NavLink>
+        </NavItem>
+
+      </Nav>
+  </>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,45 +6,23 @@ import cat1 from '../assests/cat-4912133_1280.png'
 const Header = () => {
   return (
     <>
-      <Navbar
-        className="my-2"
-        color="dark"
-        dark
-      >
+      <Navbar className="my-2" color="dark" dark >
         <NavbarBrand href="/">
-          <img
-            alt="my-Image"
-            src={cat1}
-            style={{
-              height: 40,
-              width: 40
-            }}
-          />
+          <img alt="my-Image" src={cat1} style={{height: 40, width: 40}} />
         </NavbarBrand>
       </Navbar>
-      <Navbar
-        className="my-2"
-        color="secondary"
-        dark
-      >
+
+      <Navbar className="my-2" color="secondary" dark >
         <NavbarBrand href="/">
+
           Catssss
+
         </NavbarBrand>
       </Navbar>
-      <Navbar
-        className="my-2"
-        color="dark"
-        dark
-      >
+
+      <Navbar className="my-2" color="dark" dark >
         <NavbarBrand href="/">
-          <img
-            alt="logo"
-            src={cat1}
-            style={{
-              height: 40,
-              width: 40
-            }}
-          />
+          <img alt="logo" src={cat1} style={{height: 40, width: 40}}/>
           So Many Catsss
         </NavbarBrand>
       </Navbar>
